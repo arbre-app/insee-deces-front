5eb51fc2ebc6b0e529ff560a3ea434017803d984
Florian Cassayre <florian.cassayre@gmail.com>
2021-07-27 13:59:58 +0200

    Typography fixes

--- src/form/SortOrderSelect.js
+++ src/form/SortOrderSelect.js
@@ -2,10 +2,11 @@ import PropTypes from 'prop-types';
 import { Form, InputGroup } from 'react-bootstrap';
 import { ArrowDownUp } from 'react-bootstrap-icons';
 import { Field } from 'react-final-form';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import { ORDER_TYPE_ASCENDING, ORDER_TYPE_DESCENDING } from '../api';
 
 export function SortOrderSelect({ disabled }) {
+  const intl = useIntl();
   return (
     <Field
       name="sortOrder"
@@ -26,8 +27,8 @@ export function SortOrderSelect({ disabled }) {
               disabled={disabled}
               {...input}
             >
-              <option value={ORDER_TYPE_ASCENDING}><FormattedMessage id="common.sort.ascending" /></option>
-              <option value={ORDER_TYPE_DESCENDING}><FormattedMessage id="common.sort.descending" /></option>
+              <option value={ORDER_TYPE_ASCENDING}>{intl.formatMessage({ id: 'common.sort.ascending' })}</option>
+              <option value={ORDER_TYPE_DESCENDING}>{intl.formatMessage({ id: 'common.sort.descending' })}</option>
             </Form.Control>
           </InputGroup>
         </>
