b661b47623c361196fc23b93a5ab01c2ef630b7e
Florian Cassayre <florian.cassayre@gmail.com>
2023-09-03 19:05:45 +0200

    Update config and strings

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ export const API_ENDPOINT = 'https://insee.arbre.app';
 const CONTACT_EMAIL_PREFIX = 'contact', CONTACT_EMAIL_DOMAIN = 'arbre.app';
 export const CONTACT_EMAIL = `${CONTACT_EMAIL_PREFIX}@${CONTACT_EMAIL_DOMAIN}`;
 
-export const DB_TOTAL_RECORDS = 26496121;
-export const DB_LAST_UPDATE = '2022-08-22';
+export const DB_TOTAL_RECORDS = 27159627;
+export const DB_LAST_UPDATE = '2023-07-31';
 
 export const RESULTS_PER_PAGE = [25, 50, 100];
 export const DEFAULT_RESULTS_PER_PAGE = RESULTS_PER_PAGE[0];
